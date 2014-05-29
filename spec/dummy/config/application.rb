8cc8167d75ebbb34490f8acd6aea2e0a901200a8
Matt Riemer <matthew@agilestyle.com>
2014-05-29 12:39:36 -0600

    Get working with rspec and haml

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
+require 'haml'
 require 'effective_addresses'
 require "effective_orders"
 
@@ -19,6 +20,11 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.template_engine :haml
+    end
+
   end
 end
 
