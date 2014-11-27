a001617aece530f5d17afe2f3d381fb83f08b5fc
Matt Riemer <matthew@agilestyle.com>
2014-11-27 12:22:37 -0700

    Refactor the Datatable to consider datatables_admin_path?. Version 0.4.10

--- app/models/effective/datatables/orders.rb
+++ app/models/effective/datatables/orders.rb
@@ -22,7 +22,14 @@ if defined?(EffectiveDatatables)
           price_to_currency(order.total)
         end
 
-        table_column :actions, :sortable => false, :filter => false, :partial => '/admin/orders/actions'
+        table_column :actions, :sortable => false, :filter => false do |order|
+          content_tag(:span, :style => 'white-space: nowrap;') do
+            [
+              link_to('View', (datatables_admin_path? ? effective_orders.admin_order_path(order) : effective_orders.order_path(order))),
+              (link_to('Resend Receipt', effective_orders.resend_buyer_receipt_path(order), {'data-confirm' => 'This action will email the buyer a copy of the original email receipt.  Send receipt now?'}) if order.try(:purchased?))
+            ].compact.join(' - ').html_safe
+          end
+        end
 
         def collection
           if attributes[:user_id].present?
