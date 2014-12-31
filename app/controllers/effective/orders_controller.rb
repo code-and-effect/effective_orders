6b9ddbc5ad5a1912e33f20c23a0a05c15c804bab
Matt Riemer <matthew@agilestyle.com>
2014-12-31 15:46:07 -0700

    Only save non-empty? addresses

--- app/controllers/effective/orders_controller.rb
+++ app/controllers/effective/orders_controller.rb
@@ -40,11 +40,11 @@ module Effective
 
       Effective::Order.transaction do
         begin
-          if @order.save_billing_address? && @order.user.respond_to?(:billing_address)
+          if @order.save_billing_address? && @order.user.respond_to?(:billing_address) && @order.billing_address.try(:empty?) == false
             @order.user.billing_address = @order.billing_address
           end
 
-          if @order.save_shipping_address? && @order.user.respond_to?(:shipping_address)
+          if @order.save_shipping_address? && @order.user.respond_to?(:shipping_address) && @order.shipping_address.try(:empty?) == false
             @order.user.shipping_address = @order.shipping_address
           end
 
