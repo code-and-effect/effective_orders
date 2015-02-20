0427d416c00d2ad3879ecc1aad063e6bb5ca11ab
Matt Riemer <matthew@agilestyle.com>
2015-02-20 14:32:36 -0700

    Another sanity check test

--- spec/models/factories_spec.rb
+++ spec/models/factories_spec.rb
@@ -10,4 +10,20 @@ describe 'Factories' do
   it 'should have all valid factories' do
     factories.each { |factory| FactoryGirl.create(factory).valid?.should eq true }
   end
+
+  it 'should have created an Order with a billing_address and shipping_address' do
+    order = FactoryGirl.create(:order)
+
+    order.billing_address.present?.should eq true
+    order.shipping_address.present?.should eq true
+
+    order.billing_address.valid?.should eq true
+    order.shipping_address.valid?.should eq true
+
+    order.billing_address.full_name.present?.should eq true
+    order.shipping_address.full_name.present?.should eq true
+
+    order.save.should eq true
+  end
+
 end
