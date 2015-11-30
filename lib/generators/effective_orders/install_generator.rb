84d40ae983190b8bb3a33689e5b73bcc2410b265
Matt Riemer <matthew@agilestyle.com>
2015-11-30 14:57:48 -0700

    Add effective_orders_mailer_preview

--- lib/generators/effective_orders/install_generator.rb
+++ lib/generators/effective_orders/install_generator.rb
@@ -19,6 +19,16 @@ module EffectiveOrders
         template "effective_orders.rb", "config/initializers/effective_orders.rb"
       end
 
+      def copy_mailer_preview
+        mailer_preview_path = (Rails.application.config.action_mailer.preview_path rescue nil)
+
+        if mailer_preview_path.present?
+          template 'effective_orders_mailer_preview.rb', File.join(mailer_preview_path, 'effective_orders_mailer_preview.rb')
+        else
+          puts "couldn't find action_mailer.preview_path.  Skipping effective_orders_mailer_preview."
+        end
+      end
+
       def create_migration_file
         @orders_table_name = ':' + EffectiveOrders.orders_table_name.to_s
         @order_items_table_name = ':' + EffectiveOrders.order_items_table_name.to_s
---
+++ lib/generators/templates/effective_orders_mailer_preview.rb
@@ -0,0 +1,59 @@
+# Visit http://localhost:3000/rails/mailers to see a preview of the following 3 emails:
+
+class EffectiveOrdersMailerPreview < ActionMailer::Preview
+  def order_receipt_to_buyer
+    Effective::OrdersMailer.order_receipt_to_buyer(build_preview_order)
+  end
+
+  def order_receipt_to_admin
+    Effective::OrdersMailer.order_receipt_to_admin(build_preview_order)
+  end
+
+  # This email is only sent to sellers having sold items via StripeConnect
+  def order_receipt_to_seller
+    order = build_preview_order
+    Effective::OrdersMailer.order_receipt_to_seller(order, preview_customer, order.order_items)
+  end
+
+  protected
+
+  def build_preview_order
+    order = Effective::Order.new()
+    order.user = preview_user
+    preview_order_items.each { |atts| order.order_items.build(atts) }
+    order
+  end
+
+  private
+
+  # We're building the OrderItem attributes directly
+  # so that we don't have to guess at your acts_as_purchasable object
+  def preview_order_items
+    tax_rate = (EffectiveOrders.tax_rate_method.call(Object.new()) rescue -1)
+    tax_rate = 0.05 if tax_rate < 0.0
+
+    [
+      {title: 'Item One', quantity: 2, price: 999, tax_exempt: false, tax_rate: tax_rate},
+      {title: 'Item Two', quantity: 1, price: 25000, tax_exempt: false, tax_rate: tax_rate},
+      {title: 'Item Three', quantity: 1, price: 8999, tax_exempt: false, tax_rate: tax_rate},
+      {title: 'Item Four', quantity: 1, price: 100000, tax_exempt: false, tax_rate: tax_rate}
+    ]
+  end
+
+  def preview_user
+    User.new(email: 'buyer@example.com').tap do |user|
+      user.name = 'Valued Customer' if user.respond_to?(:name=)
+      user.full_name = 'Valued Customer' if user.respond_to?(:full_name=)
+
+      if user.respond_to?(:first_name=) && user.respond_to?(:last_name=)
+        user.first_name = 'Valued'
+        user.last_name = 'Customer'
+      end
+    end
+  end
+
+  def preview_customer
+    Effective::Customer.new(user: preview_user)
+  end
+
+end
