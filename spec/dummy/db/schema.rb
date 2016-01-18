8e6ebc8ef81c94cef33814a69406503f1974a020
Andrew Chernysh <andrice@i.ua>
2016-01-18 16:57:13 +0200

    Fix broken test db for dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(:version => 4) do
     t.text     "details"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+    t.text     "note"
   end
 
   add_index "orders", ["user_id"], :name => "index_orders_on_user_id"
