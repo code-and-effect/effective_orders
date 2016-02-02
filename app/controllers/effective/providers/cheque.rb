144c54e18be793cef8554b174bb4580691c1174f
Matt Riemer <matthew@agilestyle.com>
2016-02-02 14:17:25 -0700

    typo

--- app/controllers/effective/providers/cheque.rb
+++ app/controllers/effective/providers/cheque.rb
@@ -16,7 +16,7 @@ module Effective
         if @order.save
           @order.send_pending_order_invoice_to_buyer!
           current_cart.try(:destroy)
-          flash.now[:success] = 'Successfully indicated order will be payed by cheque. Please send us a cheque.'
+          flash.now[:success] = 'Successfully indicated order will be payed by cheque. Please send a cheque.'
         else
           flash[:danger] = "Unable to save your order: #{@order.errors.full_messages.to_sentence}. Please try again."
           redirect_to effective_orders.order_path(@order)
