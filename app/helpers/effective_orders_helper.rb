eb96ea1139fa29fe82e06b30ab7ee39d89cf29e6
Matt Riemer <matthew@agilestyle.com>
2017-06-22 09:32:21 -0600

    put back price_to_currency helper

--- app/helpers/effective_orders_helper.rb
+++ app/helpers/effective_orders_helper.rb
@@ -1,4 +1,9 @@
 module EffectiveOrdersHelper
+  def price_to_currency(price)
+    price = price || 0
+    raise 'price_to_currency expects an Integer representing the number of cents' unless price.kind_of?(Integer)
+    number_to_currency(price / 100.0)
+  end
 
   def tax_rate_to_percentage(tax_rate, options = {})
     options[:strip_insignificant_zeros] = true if options[:strip_insignificant_zeros].nil?
