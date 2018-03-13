cafbf10bb477ad5dd906f1830ebc4aa040b99712
Matt Riemer <matthew@agilestyle.com>
2018-03-13 15:48:27 -0600

    checkout perfect

--- app/controllers/effective/orders_controller.rb
+++ app/controllers/effective/orders_controller.rb
@@ -16,10 +16,14 @@ module Effective
     before_action :authenticate_user!, except: [:ccbill_postback, :free, :paypal_postback, :pretend]
     before_action :set_page_title, except: [:show]
 
+    # If you want to use the Add to Cart -> Checkout flow
+    # Add one or more items however you do.
+    # redirect_to effective_orders.new_order_path, which is here.
     # This is the entry point for any Checkout button.
-    # It's basically a create action for a pending order, creating from the cart.
+    # It displayes an order based on the cart
+    # Always step1
     def new
-      @order ||= Effective::Order.new(current_cart)
+      @order ||= Effective::Order.new(view_context.current_cart)
 
       EffectiveOrders.authorize!(self, :new, @order)
 
@@ -30,9 +34,9 @@ module Effective
       end
     end
 
-    # Confirms the order
+    # Confirms an order from the cart.
     def create
-      @order ||= Effective::Order.new(current_cart)
+      @order ||= Effective::Order.new(view_context.current_cart)
       EffectiveOrders.authorize!(self, :create, @order)
 
       @order.assign_attributes(checkout_params)
@@ -45,12 +49,10 @@ module Effective
       end
     end
 
-    # Always step1
-    def edit
-      @order ||= Effective::Order.find(params[:id])
-      EffectiveOrders.authorize!(self, :edit, @order)
-    end
-
+    # If you want to use the order = Effective::Order.new(@thing); order.save! flow
+    # Add one or more items to the order.
+    # redirect_to effective_orders.order_path(order), which is here
+    # This is the entry point for an existing order.
     # Might render step1 or step2
     def show
       @order = Effective::Order.find(params[:id])
@@ -59,7 +61,13 @@ module Effective
       @page_title ||= ((@order.user == current_user && !@order.purchased?) ? 'Checkout' : @order.to_s)
     end
 
-    # Confirms the order
+    # Always step1
+    def edit
+      @order ||= Effective::Order.find(params[:id])
+      EffectiveOrders.authorize!(self, :edit, @order)
+    end
+
+    # Confirms the order from existing order
     def update
       @order ||= Effective::Order.find(params[:id])
       EffectiveOrders.authorize!(self, :update, @order)
@@ -74,6 +82,7 @@ module Effective
       end
     end
 
+    # My Orders History
     def index
       @orders = Effective::Order.deep.purchased.where(user: current_user)
       @pending_orders = Effective::Order.deep.pending.where(user: current_user)
@@ -86,11 +95,11 @@ module Effective
       @order = if params[:id].present?
         Effective::Order.find(params[:id])
       elsif current_user.present?
-        Effective::Order.purchased_by(current_user).first
+        Effective::Order.sorted.purchased_by(current_user).last
       end
 
       if @order.blank?
-        redirect_to(effective_orders.my_orders_path) and return
+        redirect_to(effective_orders.orders_path) and return
       end
 
       EffectiveOrders.authorize!(self, :show, @order)
@@ -143,10 +152,6 @@ module Effective
     end
 
     private
-
-    def current_cart
-      view_context.current_cart
-    end
 
     # StrongParameters
     def checkout_params
