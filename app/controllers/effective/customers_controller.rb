90d978c363e12e7f195a36a1e8eeb2fb3f1de983
Matt Riemer <matthew@agilestyle.com>
2018-05-18 11:44:19 -0600

    work with newer effective_resources

--- app/controllers/effective/customers_controller.rb
+++ app/controllers/effective/customers_controller.rb
@@ -13,7 +13,8 @@ module Effective
     end
 
     # I don't want save_resource to wrap my save in a transaction
-    def save_resource(resource, action)
+    def save_resource(resource, action, to_assign)
+      resource.assign_attributes(to_assign)
       resource.save!
     end
 
