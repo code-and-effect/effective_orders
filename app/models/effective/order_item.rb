209070f8302e8f9fed81cfcb6c52c70aea133768
Matt Riemer <matthew@codeandeffect.com>
2022-01-11 14:01:50 -0700

    nil fix

--- app/models/effective/order_item.rb
+++ app/models/effective/order_item.rb
@@ -68,7 +68,7 @@ module Effective
     # first or build
     def qb_item_name
       raise('expected EffectiveQbSync gem') unless defined?(EffectiveQbSync)
-      (qb_order_item || build_qb_order_item(name: purchasable.qb_item_name)).name
+      (qb_order_item || build_qb_order_item(name: purchasable&.qb_item_name)).name
     end
 
   end
