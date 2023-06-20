750d60a0c920c2262025ef0569b19baad92b3de2
Matt Riemer <matthew@codeandeffect.com>
2023-06-20 10:39:50 -0600

    add decline to pretend form

--- app/controllers/effective/providers/pretend.rb
+++ app/controllers/effective/providers/pretend.rb
@@ -10,13 +10,22 @@ module Effective
 
         EffectiveResources.authorize!(self, :update, @order)
 
-        order_purchased(
-          payment: 'for pretend',
-          provider: 'pretend',
-          card: 'none',
-          purchased_url: pretend_params[:purchased_url],
-          current_user: current_user
-        )
+        if params[:commit].to_s.include?('Decline')
+          order_declined(
+            payment: 'for pretend', 
+            provider: 'pretend', 
+            card: 'none', 
+            declined_url: pretend_params[:declined_url]
+          )
+        else
+          order_purchased(
+            payment: 'for pretend', 
+            provider: 'pretend', 
+            card: 'none', 
+            purchased_url: pretend_params[:purchased_url],
+            current_user: current_user
+          )
+        end
       end
 
       def pretend_params
